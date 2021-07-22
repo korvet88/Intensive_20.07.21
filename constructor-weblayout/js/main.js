991e9aa14597688344afde4663e97cd6d0adf94b
korvet88 <regim88@yandex.ru>
2021-07-22 17:11:57 +0700

    Работоспособная плюс иконки соцсетей на странице.
    Добавлены:
    * Список соц сетей
    * Условие для обнаружения добавленных соц сетей
    *В функции получения элементов 3 сво-во атрибуты, через .map в атрибуты попадает список.
    *Перебор атрибутов, для поиска и вывода

--- constructor-weblayout/js/main.js
+++ constructor-weblayout/js/main.js
@@ -23,18 +23,26 @@ menuButton.addEventListener('click', function () {
 	menu.classList.toggle('header-active');
 })
 */
-const getElement = (tagName, classNames) => {
+const getElement = (tagName, classNames, attributes) => {
 	const element = document.createElement(tagName);
 
 	if (classNames) {
 		element.classList.add(...classNames);
 	}
+
+	if (attributes) {
+		for (const attribute in attributes) {
+			console.log(attribute);
+			element[attribute] = attributes[attribute];
+		}
+	}
+
 	return element;
 };
 
 const createHeader = (param) => {
 	const header = getElement('header');
-	const container = getElement('div', ['container'])
+	const container = getElement('div', ['container']);
 	const wrapper = getElement('div', ['header']);
 
 	if (param.header.logo) {
@@ -43,7 +51,23 @@ const createHeader = (param) => {
 		logo.alt = 'Логотип ' + param.title;
 		wrapper.append(logo);
 	}
+	if (param.header.social) {
+		const socialWrapper = getElement('div', ['social']);
+		const allSocial = param.header.social.map(item => {
+			const socialLink = getElement('a', ['social-link']);
+			socialLink.append(getElement('img', [], {
+				src: item.image,
+				alt: item.title,
+			}));
 
+			socialLink.href = item.link;
+
+			return socialLink;
+		});
+		console.log(allSocial);
+		socialWrapper.append(...allSocial);
+		wrapper.append(socialWrapper);
+	}
 	header.append(container);
 	container.append(wrapper);
 
@@ -64,6 +88,24 @@ const movieConstructor = (selector, options) => {
 movieConstructor('.app', {
 	title: 'Ведьмак',
 	header: {
-		logo: 'witcher/logo.png'
+		logo: 'witcher/logo.png',
+		social: [
+			{
+				title: 'Twitter',
+				link: 'https://twitter.com',
+				image: 'witcher/social/twitter.svg',
+			},
+			{
+				title: 'Instagram',
+				link: 'http//instagram.com',
+				image: 'witcher/social/instagram.svg',
+			},
+			{
+				title: 'Facebook',
+				link: 'https://facebook.com',
+				image: 'witcher/social/facebook.svg',
+			}
+		]
+
 	} 
 });
